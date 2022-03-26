7b410e9ed206d1693aa26bc9f4d9cbf6a0614bb4
Mukhammad Akilov <muhammad_akilov@mail.ru>
2022-03-27 01:06:02 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
+import {ReactComponent as SvgMap} from './assets/automatika.svg';
 
-function App() {
+const mapTitles = new Map();
+mapTitles.set(1, "First title first title")
+         .set(2, "Second title second title")
+         .set(3, "Third title third title")
+         .set(4, "Fourth title fourth title")
+         .set(5, "Fifth title fifth title")
+         .set(6, "Sixth title sixth title")
+         .set(7, "Seventh title seventh title")
+         .set(8, "Eighth title eight title")
+         .set(9, "Nineth title nineth title");
+
+const App = () =>  {
+    const [selectedTitle, setSelectedTitle] = useState({
+        id: 1,
+        title: mapTitles.get(1)
+    })
+
+    const clearActiveTitles = (svgTitles) => {
+        for(let svgTitle of svgTitles) {
+            svgTitle.classList.remove("roadmap-title-active");
+        }
+    }
+
+    const handleSvgClick = (event) => {
+        const svgElem = document.getElementById("automatika-roadmap");
+        const svgTitles = svgElem.querySelectorAll(".roadmap-title");
+
+        for(let svgTitle of svgTitles) {
+
+            svgTitle.addEventListener("click", event => {
+                // Clear all active titles
+                clearActiveTitles(svgTitles);
+                svgTitle.classList.add("roadmap-title-active");
+
+                const [_ignore, _ignore2, id] = svgTitle.dataset.value.split("-");
+                setSelectedTitle({
+                    id: parseInt(id),
+                    title: mapTitles.get(parseInt(id)),
+                })
+            })
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("load", handleSvgClick);
+
+        return () => {
+            window.removeEventListener("load", handleSvgClick);
+        }
+    }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app container">
+        <SvgMap />
+        <div>
+            <h1 className="title">
+                {selectedTitle.title}
+            </h1>
+        </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders svg element', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const svgElement = screen.getByTestId("automatika-roadmap");
+  expect(svgElement).toBeInTheDocument();
 });
